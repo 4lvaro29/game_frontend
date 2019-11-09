3fdedba1fa57e09ac8f387cf381e568a19288e49
alvaro <4lvaro.up@gmail.com>
2019-11-09 13:40:57 -0300

    ADD: angular main component, routes, custom styles and appmodule configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { MainComponent } from './components/main/main.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material/input'; 
+import { MatFormField, MatFormFieldControl, MatFormFieldModule } from '@angular/material/form-field';
+import {MatCardModule} from '@angular/material/card'; 
+import {MatButtonModule} from '@angular/material/button'; 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatCardModule,
+    MatButtonModule,
   ],
-  providers: [],
+  providers: [{ provide: MatFormFieldModule, useValue: {} }, { provide: MatFormField, useValue: [] }, { provide: MatFormFieldControl, useValue: [] }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/main/main.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.css']
+})
+export class MainComponent implements OnInit {
+  name = new FormControl('');
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
